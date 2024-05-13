5bbb97f564f59a0b78f2d36ce3570873c8a9dcd4
vaffatato <vincenzoaffatato929@gmail.com>
2024-05-13 13:40:14 +0200

    ADD dependency giuste per swagger + classe che legge le config del progetto

---
+++ tyb_backend/src/main/java/com/tyb/tyb_backend/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.tyb.tyb_backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Servizi per la piattaforma di VideoOnDemand");
+        Contact contact = new Contact();
+        contact.setEmail("vincenzoaffatato929@gmail.com");
+        contact.setName("Vincenzo");
+        contact.setUrl("https://localhost:8080/");
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+        Info info = new Info()
+                .title("Project videOnDemand")
+                .version("1.0")
+                .contact(contact)
+                .description("Piattaforma di videoOnDemand").termsOfService("https://www.affvinc.com/terms")
+                .license(mitLicense);
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
+}
+
