608704b7b7e3150ebe210c9189e6e6af6d970652
giorgio.contini <giorgio.contini@dxc.com>
2024-06-24 17:03:30 +0200

    fix: timer

--- tyb_frontend/src/components/TimerComponent/TimerComponent.tsx
+++ tyb_frontend/src/components/TimerComponent/TimerComponent.tsx
@@ -12,10 +12,11 @@ const TimerComponent: React.FC<TimerProps> = ({ onTimeout, remainingTime, setRem
 
 
     useEffect(() => {
+        debugger
         const timerId = setInterval(() => {
             setRemainingTime(prevTime => {
                 const newTime = prevTime - 1;
-                if (newTime <= 0) {
+                if (newTime === 0) {
                     clearInterval(timerId);
                     onTimeout();
                 }
@@ -27,8 +28,12 @@ const TimerComponent: React.FC<TimerProps> = ({ onTimeout, remainingTime, setRem
     }, [onTimeout]);
 
     const formatTime = (time: number): string => {
-        const minutes = Math.floor(time / 60);
-        const seconds = time % 60;
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        if (time<=0){
+            minutes=0;
+            seconds=0;
+        }
         return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     };
 
